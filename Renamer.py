3d58f45ee83f12cc345b08974923e55970375697
Jasper <jasper.yun@hotmail.com>
2019-11-20 15:53:35 -0500

    Commented code

--- Renamer.py
+++ Renamer.py
@@ -1,24 +1,44 @@
-# for manipulating the file system, etc.
+
+"""
+This program batch renames files. The user specifies a directory that must
+be a subdirectory of the Desktop; this is required so that critical files
+are not accidentally renamed.
+
+Once the directory is specified, the user can specify a format for which
+all of the files will be renamed to. An index is appended to the end of
+the given format, and then the file is renamed. Note that directories
+within the specified directory will be ignored.
+
+After the renaming process, a function runs to check that the files were
+renamed properly.
+
+The program contains code to allow cancellation of certain steps in the
+renaming procedure.
+
+Author: Jasper Yun
+Date:   2019-11-20
+"""
+
 import os
 
 def RemoveSpace(string):
     """This function takes an input of a string. It outputs a string without any spaces
         at the beginning or end of the string."""
     if len(string)==0:
-        pass                    #if string input is empty, then don't do anything
-    elif (string[-1]==' '):     #runs only if the last character in the string is a space
-        string = string[:-1]    #removes the space
-        string=RemoveSpace(string)     #runs recursively to ensure it runs completely
-    elif (string[-1]=='\t'):    #if last character in string is a tab-space
-        string = string[:-1]    #remove the tab
+        pass                    # if string input is empty, then don't do anything
+    elif (string[-1]==' '):     # runs only if the last character in the string is a space
+        string = string[:-1]    # removes the space
+        string=RemoveSpace(string)     # runs recursively to ensure it runs completely
+    elif (string[-1]=='\t'):    # if last character in string is a tab-space
+        string = string[:-1]    # remove the tab
         string=RemoveSpace(string)
-    elif (string[0]==' '):      #runs only if the first character in the string is a space
-        string = string[1:]     #removes the space
-        string=RemoveSpace(string)     #recursive to ensure all spaces are removed
+    elif (string[0]==' '):      # runs only if the first character in the string is a space
+        string = string[1:]     
+        string=RemoveSpace(string)     # recursive to ensure all spaces are removed
     elif (string[0]=='\t'):
-        string = string[1:]     #if first character in string is a tab-space
-        string=RemoveSpace(string)     #remove tab
-    return string               #returns a string without a space at the end
+        string = string[1:]     # if first character in string is a tab-space
+        string=RemoveSpace(string)     # recursive to remove all tabs
+    return string               # returns a string without a space at the end
 
 def checkNonEmptyInput(string):
     """Checks that the input is not an empty string or that the input is not
@@ -35,7 +55,7 @@ def checkProceed():
     print("Are you sure you want to proceed? Enter: \n")
     print("1 -- proceed with renaming")
     print("2 -- abort current operation")
-    choice = chooseOption()     # input sanitization
+    choice = chooseOption()                 # input sanitization
     return choice
 
 def chooseOption():
@@ -46,7 +66,7 @@ def chooseOption():
             choice = int(choice)
             if choice == 1 or choice == 2:
                 break
-            else:
+            else:       # pass returns to top of while loop, allowing new option choice
                 pass
         except ValueError:
             print("illegal input\n")
@@ -75,29 +95,27 @@ def chooseParams(safePath):
                          " files to be renamed: ")
             path = os.path.join(safePath, path)
             
-            # using lower() makes directory path case insensitive;
             # check to see if the path exists
             if os.path.exists(path) == False:
                 print("Non-existent path\n")
-            elif safePath.lower() in path.lower():
+            elif safePath.lower() in path.lower(): # lower() makes paths case-insensitive
                 print("\nThe current path is: " + path.lower())
                 choice = checkProceed()
                 if choice == 1:
                     break
-                if choice == 2:
+                if choice == 2: # pass returns to top of while loop - choose new path
                     pass
         try:
             os.chdir(path)  # change directory to the one specified by the path
-            #dirName = path[path.rfind('\\')+1:len(path)] # get the directory name
             break
-        except OSError:
+        except OSError:     # if path does not exist, catch the error
             print("Non-existent path\n")
 
     # input the file names format
     while (True):
         newName = input("\nEnter the format of the new names: ")
-        stripped = checkNonEmptyInput(newName)
-        if stripped != "empty":
+        stripped = checkNonEmptyInput(newName) # input sanitization
+        if stripped != "empty":                # non-empty input, so exit while loop
             break
     return path, newName
 
@@ -114,6 +132,7 @@ def rename(path, newName):
     i = 1
     # rename each file:
     for filename in os.listdir(path):
+        # check that we are renaming FILES only, not folders
         if (os.path.isfile(os.path.join(path, filename))):
             ext = filename[filename.rfind('.'): len(filename)]
             dst = newName + "-" + str(i) + ext
@@ -121,8 +140,8 @@ def rename(path, newName):
             dst = os.path.join(path, dst)
 
             os.rename(src,dst)
-            i += 1
-        else:
+            i += 1  # incrememnt the index
+        else:       # for objects that are not files, we simply pass
             pass
 
 def successRename(path, newName):
@@ -132,15 +151,14 @@ def successRename(path, newName):
     renamed = os.listdir(path)  # this is a list
     toRem = []                  # empty list of strings to be removed from renamed
     for i in renamed:
-        if os.path.isdir(os.path.join(path, i)):
+        if os.path.isfile(os.path.join(path, i)) == False: # deals with *any* non-files
             toRem.append(i)
     for i in toRem:
         renamed.remove(i)
 
-    renamed.sort(key = len)    # sort list by the index
-    
+    renamed.sort(key = len)     # sort list by the index
     i = 1
-    flag = True                # to check if the rename was successful
+    flag = True                 # to check if the rename was successful
     for j in range(len(renamed)):
         nameFormat = newName + "-" + str(i)
         if nameFormat in renamed[j]:
@@ -148,7 +166,7 @@ def successRename(path, newName):
             pass
         else:
             print(str(i) + " -- " + nameFormat + " failed")
-            flag = False
+            flag = False        # return false if any renamed files do not match
         i += 1
     return flag
 
@@ -159,23 +177,27 @@ while (True):
     user = input("Enter your username: ")
 
     # check if user exists:
-    if (os.path.exists(os.path.join("C:\\users\\",user))):  # user exists
+    if (os.path.exists(os.path.join("C:\\users\\",user))): 
         print('')
         break
     else:
         print("User does not exist\n")
 
 # we require that the directory containing the files to be renamed
-# be at subdirectory of the Desktop; this is for safety reasons
+# be a subdirectory of the Desktop; this is for safety reasons
 safePath = os.path.join("C:\\Users\\", user, "desktop")
 
 while (True):
-    helper()                # print the "menu"
+    helper()        # print the "menu"
     choice = chooseOption()
-    if choice == 1:
-        (path, newName) = chooseParams(safePath)
+    
+    if choice == 1: # enter the renaming process
+        
+        (path, newName) = chooseParams(safePath)    # choose parameters
         print("You are about to rename the files in: \t " + path)
         print("to the format: \t " + newName + "-(index)\n")
+
+        # let user confirm and proceed to renaming
         go = checkProceed()
         if go == 1:
             rename(path, newName)
@@ -189,7 +211,7 @@ while (True):
         else:
             print("\nRename CANCELLED")
         print("\n")
-
-    elif choice == 2:
+    
+    elif choice == 2: # exit the program
         print("\nProgram terminated.")
         break
